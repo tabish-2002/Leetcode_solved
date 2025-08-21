dd7317c7443dd5239a4a3f7aac4cb0451902f88d
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2025-08-21 12:53:24 -0400

    Time: 2 ms (42.12%), Space: 58.9 MB (8.76%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -3,29 +3,19 @@
  * @return {string[]}
  */
 var fizzBuzz = function(n) {
-       var res = [];
-    var i = 1, fizz = 0, buzz = 0;
+    let arr = [];
 
-    while (i <= n) {
-        fizz++;
-        buzz++;
-
-        if (fizz === 3 && buzz === 5) {
-            res.push("FizzBuzz");
-            fizz = buzz = 0;
-        } else if (fizz === 3) {
-            res.push("Fizz");
-            fizz = 0;
-        } else if (buzz === 5) {
-            res.push("Buzz");
-            buzz = 0;
+    for (let i = 1; i <= n; i++) {
+        if (i % 15 === 0) {
+            arr.push("FizzBuzz");
+        } else if (i % 5 === 0) {
+            arr.push("Buzz");
+        } else if (i % 3 === 0) {
+            arr.push("Fizz");
         } else {
-            res.push(i.toString());
+            arr.push(i.toString());
         }
-
-        i++;
     }
 
-    return res;
+    return arr; 
 };
-    

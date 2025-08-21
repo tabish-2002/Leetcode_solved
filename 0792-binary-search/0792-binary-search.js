2a6245a6da5e48cc335586bad9a5dd1589f28e83
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2025-08-21 13:25:46 -0400

    Time: 0 ms (100%), Space: 58.2 MB (40.38%) - LeetHub

--- 0792-binary-search/0792-binary-search.js
+++ 0792-binary-search/0792-binary-search.js
@@ -4,21 +4,20 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let left = 0;
-    let right = nums.length -1;
+    let left = 0;// start at first index
+    let right = nums.length -1
 
-    while (left<=right){
-        let mid = Math.floor((left+right)/2);
-        // if the target is the mid value
-        if((nums[mid]=== target)) return mid;
+    while (left<= right){
+        let mid = Math.floor((left + right )/2);
 
-        // if the value is less then mid
-        if(target> nums[mid]){
-            left = mid +1;
+        if(nums[mid]=== target){
+            return mid;
+        }
+        else if(nums[mid]< target){
+             left = mid +1;
         } else {
             right = mid -1;
         }
-
     }
-    return -1
+    return -1; /// if does not exist
 };

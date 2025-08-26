5f66827350854d6602d1584348b7ecbe93561aa8
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2025-08-26 17:56:01 -0400

    Time: 0 ms (100%), Space: 53 MB (83.95%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    let n = digits.length;
+
+    for (let i = n-1; i>= 0; i--){
+        if (digits[i] < 9){
+            digits[i] += 1;
+            return digits;
+        }
+        digits[i] = 0;
+    }
+    digits.unshift(1)
+    return digits;
+};

fbae8afe876ed437cd6cce9a7ee0118c3cc8cfd1
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2025-09-02 09:16:50 -0400

    Time: 1 ms (84.32%), Space: 58.2 MB (41.23%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -5,17 +5,19 @@
 var fizzBuzz = function(n) {
     let arr = [];
 
-    for (let i = 1; i <= n; i++) {
-        if (i % 15 === 0) {
-            arr.push("FizzBuzz");
-        } else if (i % 5 === 0) {
-            arr.push("Buzz");
-        } else if (i % 3 === 0) {
-            arr.push("Fizz");
+    for (let i = 1; i<=n; i ++){// since we are using a array that is not given to us, we use n no lenght of n
+        if(i % 3 === 0 && i % 5 === 0 ){/* we are using this 15 first because we need this condition to be
+        checked first, else the previous statements will run, if we dont do this then the condition will not 
+        be able to catch all the problems that are given to us
+        */
+            arr.push("FizzBuzz")
+        }else if(i % 3 === 0){
+            arr.push("Fizz")      
+        }else if(i % 5 === 0){
+            arr.push("Buzz")  
         } else {
-            arr.push(i.toString());
+            arr.push(i.toString())
         }
     }
-
-    return arr; 
+    return arr;
 };

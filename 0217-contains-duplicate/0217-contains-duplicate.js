b7afdbab3042a2b6dbf004201769e73f5b3c986c
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2025-09-02 20:42:59 -0400

    Time: 17 ms (70.02%), Space: 74.1 MB (52.07%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,10 +3,14 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-  let uniqueNums = new Set(nums);
+    // lets just store all variable seen in a hashset, set only allows unique values so it is best
+    let seen = new Set();
 
-    if(uniqueNums.size === nums.length)
-        return false;
-    else
-        return true;
-};  
+    for(let val of nums){
+        if(seen.has(val)){// cant write reverse because we will stop at the first new num
+            return true;
+        } 
+         seen.add(val); // we need to add it if we dont see it
+    }
+    return false// out side the for loop
+};

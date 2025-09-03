6445f06e93bdbdbb784e5b60cf7f3ed39be5dce2
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2025-09-02 20:03:25 -0400

    Time: 1 ms (88.17%), Space: 53.8 MB (90.54%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,15 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let map = new Map();// we are declaring a new map variable, so let is used
-    // since we are gicen an array, we use the length function
-    for(let i = 0; i<nums.length; i ++){
-        let compliment = target - nums[i];
-        if(map.has(compliment)){
-            return [map.get (compliment), i];
-        }
-        map.set(nums[i], i)// need set both index and value
-    }
-    
+    let map = new Map() // create a new varibale for map
+
+    for(let i = 0; i<nums.length; i++){
+        let otherInt = target - nums[i]; // [] represent the values of i at that index
+     if(map.has(otherInt)){
+        return [map.get(otherInt), i]; // return the index which is denoted by the i wihtout square brackets
+     }// we use [] to put into array over here
+     map.set(nums[i], i); // this puts the entire value and index into the array, [i] is the value while i is the index
+    }// inside the loop because we need to make sure if we have not seen this value
 };
-// use () for function, and [] for index values

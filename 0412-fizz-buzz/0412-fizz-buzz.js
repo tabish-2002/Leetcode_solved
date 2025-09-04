29b38cdccacaf0645c9d7d81be9a94ecd0145d98
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2025-09-04 11:51:17 -0400

    Time: 3 ms (13.93%), Space: 58.4 MB (29.75%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -3,19 +3,18 @@
  * @return {string[]}
  */
 var fizzBuzz = function(n) {
-    // since we are returning an empty arr, create an arr to push values into a arr
-    let newArray = [];
-
-    for(let i = 1; i <= n; i++){// start at 1, becuase it says 1 indexed
+    let arrAnswer = [];
+    
+    for (let i = 1; i <=n; i++){
         if (i % 3 === 0 && i % 5 === 0){
-            newArray.push("FizzBuzz")// print fizzbuzz first because all other numbers will not be printed
-        } else if (i % 3 === 0 ){
-            newArray.push("Fizz")
+            arrAnswer.push("FizzBuzz")
+        } else if (i % 3 === 0){
+           arrAnswer.push("Fizz") 
         } else if (i % 5 === 0){
-            newArray.push("Buzz")
+            arrAnswer.push("Buzz") 
         } else{
-            newArray.push(i.toString()) // add brackets at the end
+            arrAnswer.push(i.toString())
         }
     }
-    return newArray;
+    return arrAnswer;
 };

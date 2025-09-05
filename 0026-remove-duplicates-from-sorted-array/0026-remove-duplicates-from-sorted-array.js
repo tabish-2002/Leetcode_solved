347ec0dd64a95eb1cdcda77ede80e9ee109d00f0
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2025-09-05 16:24:11 -0400

    Update 0026-remove-duplicates-from-sorted-array.js

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -5,7 +5,7 @@
 var removeDuplicates = function(nums) {
     let n = nums.length, l=0, r=0;
     while (r<n){
-        nums[l] = nums [r]; // skip same calues
+        nums[l] = nums [r]; // skip same values
         while (r < n && nums [r]=== nums [l]){
             r++
         }

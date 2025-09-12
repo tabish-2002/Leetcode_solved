1831a9ee99e474f40ec38d59bcd347eb8f0e8b18
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2025-09-12 18:49:12 -0400

    Time: 1 ms (88.03%), Space: 58.3 MB (5.85%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,14 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const map = new Map(); // stores elements, allows to look up in o(1)
+    let map = new Map()
 
-    for (let i = 0; i <nums.length; i++){
-        let otherInt= target - nums[i];
+    for(let i = 0; i<nums.length; i++){
+        let otherInt = target - nums[i];
         if(map.has(otherInt)){
-            return [map.get(otherInt), i]// return answer in array, we use []
+            return [map.get(otherInt), i]
         }
-        map.set(nums[i], i);
+        map.set(nums[i], i)
     }
     
 };

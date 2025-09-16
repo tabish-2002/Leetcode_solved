3ff5f9976defe9104a1a94dfbb5d4751258c39b3
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2025-09-16 12:02:44 -0400

    Time: 36 ms (45.41%), Space: 67.7 MB (58.95%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+      let map = new Map();
+
+  for (let word of strs) {
+    let key = word.split('').sort().join('');
+    if (!map.has(key)) map.set(key, []);
+    map.get(key).push(word);
+  }
+
+  return Array.from(map.values());
+    
+};

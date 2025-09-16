b846b719cb7d05fc081e5de61e6b3980c33c81e4
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2025-09-16 11:48:02 -0400

    Time: 0 ms (100%), Space: 54 MB (65.32%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+      if (!strs || strs.length === 0) return "";
+  let prefix = strs[0];
+  for (let i = 1; i < strs.length; i++) {
+    while (!strs[i].startsWith(prefix)) {
+      prefix = prefix.slice(0, -1);
+      if (prefix === "") return "";
+    }
+  }
+  return prefix
+};

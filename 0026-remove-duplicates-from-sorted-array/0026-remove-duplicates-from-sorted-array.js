2502b5fd735f04c15ae0e7ba0e2543dda120c5f7
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2025-09-19 14:17:39 -0400

    Time: 0 ms (100%), Space: 58.3 MB (30.25%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,13 +3,14 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    let n = nums.length, l=0, r=0;
-    while (r<n){
-        nums[l] = nums [r]; // skip same values
-        while (r < n && nums [r]=== nums [l]){
-            r++
+    if (nums.length === 0) return 0; // edgecase
+    let k = 0;
+
+    for(let i = 0; i<nums.length; i++){
+        if (nums[i] !== nums[k]){
+            k ++;
+            nums[k] = nums[i];
         }
-          l++
     }
-    return l
+    return k+1;
 };

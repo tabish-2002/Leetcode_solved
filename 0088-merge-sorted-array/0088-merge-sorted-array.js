e2781153a63d51b7e80f6a5064fb4e41d54fe80b
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2025-09-28 09:49:09 -0400

    Time: 0 ms (100%), Space: 54.6 MB (58%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -6,27 +6,26 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    let i = m - 1;       // last element in nums1's valid part
-    let j = n - 1;       // last element in nums2
-    let k = m + n - 1;   // last position in nums1
+    let i = m - 1;      // last valid element in nums1
+    let j = n - 1;      // last element in nums2
+    let k = m + n - 1;  // last position in nums1
 
+    // Merge from the end
     while (i >= 0 && j >= 0) {
         if (nums1[i] > nums2[j]) {
-            nums1[k] = nums1[i]; // place the larger value at k
+            nums1[k] = nums1[i];
             i--;
         } else {
-            nums1[k] = nums2[j]; // place nums2[j] at k
+            nums1[k] = nums2[j];
             j--;
         }
         k--;
     }
 
-    // Copy remaining nums2 elements (if any)
+    // If any elements left in nums2, copy them
     while (j >= 0) {
         nums1[k] = nums2[j];
         j--;
         k--;
     }
 };
-
-    

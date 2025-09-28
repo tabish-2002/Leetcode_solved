fe7fcbe508f7fa633c1080a38665b8cd1f55e95e
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2025-09-28 10:13:06 -0400

    Time: 0 ms (100%), Space: 65.8 MB (23.88%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+      let left = 0;           // buy day
+    let right = 1;          // sell day
+    let maxProfit = 0;
+
+    while (right < prices.length) {
+        if (prices[right] > prices[left]) {
+            maxProfit = Math.max(maxProfit, prices[right] - prices[left]);
+        } else {
+            left = right; // new minimum price to buy
+        }
+        right++;
+    }
+
+    return maxProfit;
+    
+};

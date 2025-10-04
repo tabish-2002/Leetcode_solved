e916e8f0a2bc694378ae7eec8ec73d827cece32d
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2025-10-03 22:53:10 -0400

    Time: 1 ms (76.96%), Space: 54.7 MB (66.1%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var calPoints = function(operations) {
+    let stack = [];
+
+    for (let i of operations){
+        if (i === '+'){
+            stack.push(stack[stack.length -1] + stack[stack.length -2])
+        } else if(i === 'D'){
+            stack.push(2* stack[stack.length -1])
+        } else if (i === 'C'){
+            stack.pop()
+        }else{
+            stack.push(parseInt(i))
+        }
+    }
+    return stack.reduce((a,b)=> a+b,0);
+};

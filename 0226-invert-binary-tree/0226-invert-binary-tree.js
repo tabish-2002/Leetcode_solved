01af15b03f277b2d53ebe158ed112dd4dab906e4
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2025-10-13 16:48:16 -0400

    Time: 0 ms (100%), Space: 55.1 MB (41.87%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.js
+++ 0226-invert-binary-tree/0226-invert-binary-tree.js
@@ -11,14 +11,14 @@
  * @return {TreeNode}
  */
 var invertTree = function(root) {
-    if(root === null) return null;
+    if (root === null) return null;
 
     let temp = root.left;
     root.left = root.right;
     root.right = temp;
 
-    invertTree(root.right);
     invertTree(root.left);
+    invertTree(root.right);
 
     return root;
     

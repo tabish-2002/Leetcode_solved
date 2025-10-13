e21dfc12aaecb66ae594fc932ceed5e593f4bfa0
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2025-10-12 20:46:03 -0400

    Time: 1 ms (61.36%), Space: 56.4 MB (91.07%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -10,7 +10,7 @@ var fizzBuzz = function(n) {
             arr.push("FizzBuzz")
         } else if(i %5 ===0){
             arr.push("Buzz")
-        }else if (i %3 ===0){
+        }else if (i % 3 ===0){
             arr.push("Fizz")
         } else{
             arr.push(i.toString())

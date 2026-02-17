7b760223f0d832368a4a44fccbf667e8d0e24358
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2026-02-17 15:59:31 -0500

    Time: 0 ms (100%), Space: 57.9 MB (61.39%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -5,12 +5,12 @@
 var removeDuplicates = function(nums) {
     let k = 0;// will act as the first pointer
 
-    for (let i = 0; i <nums.length; i++){// go tjru array
-        if (nums[i] !== nums[k-1]){// if the values are not equa. this compares the last 2 values
-            nums[k] = nums[i]// we set nums of k to the values of I
-            k++;// set the k pointer forward
+    for (let i = 0; i <nums.length; i++){// go thru the array
+        if (nums[i] !== nums[k-1]){// if the last 2 values are not equal
+            nums[k] = nums[i]// we set the values to I
+            k++;// set the values forward
         }
     }
-    return k;// return the k pointer after we have gone thru the array
-    
+    return k;
+    // we return the k pointer
 };

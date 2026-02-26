057c6d856000d5f5f941e69583b4ae4902835633
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2026-02-26 14:44:28 -0500

    Time: 0 ms (100%), Space: 57.5 MB (86.77%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -4,22 +4,19 @@
  * @return {number}
  */
 var search = function(nums, target) {
-        let left = 0;
-    let right = nums.length - 1;  // Add 'let'
-    
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
-        
-        if (nums[mid] === target) {  // Compare VALUES, not indices
-            return mid;  // Found it! Return the index
-        } else if (nums[mid] < target) {
-            // Target is in the right half
+    let left = 0;
+    let right = nums.length -1;
+
+    while (left <= right){
+        let mid = Math.floor((left + right)/2);
+
+        if (nums[mid] === target){
+            return mid;
+        } else if(nums [mid] < target){
             left = mid + 1;
-        } else {
-            // Target is in the left half
-            right = mid - 1;
+        }else{
+            right = mid -1;
         }
     }
-    
-    return -1;  // Target not found
+    return -1; 
 };

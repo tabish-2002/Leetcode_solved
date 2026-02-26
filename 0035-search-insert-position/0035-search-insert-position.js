4c8db6f03d10a6b11830791bc9fc0b6d274736f6
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2026-02-26 15:19:44 -0500

    Time: 0 ms (100%), Space: 53.4 MB (86.24%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -8,14 +8,15 @@ var searchInsert = function(nums, target) {
     let right = nums.length -1;
 
     while (left <= right){
-        let mid = Math.floor((left + right)/2);
-        if(nums [mid]=== target){
+        let mid = Math.floor((right + left) /2);
+
+        if (nums[mid]=== target){
             return mid;
-        } else if (nums [mid] > target){
-            right = mid - 1;
-        } else{
+        }else if (nums[mid] < target){
             left = mid + 1;
+        }else{
+            right = mid -1;
         }
     }
-    return left;
+    return left;// put into the lowest place possible
 };

f9af30c925b851dd6d5e3145abe466263995e444
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2026-02-27 20:06:11 -0500

    Time: 40 ms (69.66%), Space: 53.3 MB (79.8%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.js
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.js
@@ -15,16 +15,18 @@ var guessNumber = function(n) {
     let left = 1;
     let right = n;
 
-    while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
-        let result = guess(mid);
+    while (left <= right){
+        let mid = Math.floor((left + right)/2)
+        let result = guess(mid)// declare mid into guess API
 
-        if (result === 0) {
-            return mid;           // ✅ found the number
-        } else if (result === -1) {
-            right = mid - 1;      // ❌ guess too high → go left
-        } else {
-            left = mid + 1;       // ❌ guess too low → go right
+
+        if(result === 0){
+            return mid;
+        } else if (result === 1){
+            left = mid + 1;
+        } else{
+            right = mid - 1;
         }
     }
+    
 };

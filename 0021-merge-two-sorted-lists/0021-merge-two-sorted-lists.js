35b2aab92cc735cf45e361bb3378b80affff98e4
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2026-02-28 16:33:55 -0500

    Time: 0 ms (100%), Space: 58 MB (36.03%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -11,22 +11,20 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-        let dummy = new ListNode(-1);
+    let dummy = new ListNode(-1);
     let current = dummy;
 
-    while (list1 && list2) {
-        if (list1.val < list2.val) {
+    while (list1 && list2){
+        if (list1.val < list2.val){
             current.next = list1;
             list1 = list1.next;
-        } else {
+        }else{
             current.next = list2;
             list2 = list2.next;
         }
         current = current.next;
     }
-
     current.next = list1 || list2;
 
     return dummy.next;
-    
 };

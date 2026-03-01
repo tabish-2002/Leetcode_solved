b280a9e5ac8ab916ed1e28c7e2dec16c415c5c6a
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2026-03-01 17:06:25 -0500

    Time: 64 ms (10.92%), Space: 59.6 MB (34.94%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -11,12 +11,13 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    let fast = head;
     let slow = head;
+    let fast = head;
 
     while (fast && fast.next){
         slow = slow.next;
-        fast = fast.next.next;
+        fast = fast.next.next
+
 
         if (slow === fast){
             return true;

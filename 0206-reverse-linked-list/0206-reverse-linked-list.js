24b3dae00e5a802ba19b58994d3a5091c1b5deab
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2026-03-02 16:06:18 -0500

    Time: 0 ms (100%), Space: 58.6 MB (8.29%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.js
+++ 0206-reverse-linked-list/0206-reverse-linked-list.js
@@ -10,11 +10,13 @@
  * @return {ListNode}
  */
 var reverseList = function(head) {
-    if (!head|| !head.next) return head;
-
+    if (!head || !head.next){
+        return head;
+    }
     let newHead = reverseList(head.next);
-    head.next.next= head;
+    head.next.next = head;
     head.next = null;
+
 
     return newHead;
 };

11d16cad05e8634d0036f5c7eeab9931c0804a81
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2026-03-11 11:46:04 -0400

    Time: 2 ms (33.35%), Space: 62.9 MB (37.69%) - LeetHub

---
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var diameterOfBinaryTree = function(root) {
+    let diameter = 0;
+
+    function height (node){
+        if (!node) return 0;
+
+        let rightHeight = height(node.right);
+        let leftHeight = height(node.left);
+
+        diameter = Math.max(diameter, leftHeight+ rightHeight);
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+    height(root);
+    return diameter;
+};

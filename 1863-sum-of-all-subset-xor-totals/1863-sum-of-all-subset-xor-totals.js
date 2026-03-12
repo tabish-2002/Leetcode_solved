436cee99844994453f2e4f0459f759017d8c86d5
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2026-03-12 15:12:45 -0400

    Time: 7 ms (21.66%), Space: 54 MB (59.24%) - LeetHub

---
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var subsetXORSum = function(nums) {
+        const n = nums.length;
+    let total = 0;
+    for (let mask = 0; mask < (1 << n); mask++) {
+        let subsetXor = 0;
+        for (let i = 0; i < n; i++) {
+            if (mask & (1 << i)) {
+                subsetXor ^= nums[i];
+            }
+        }
+        total += subsetXor;
+    }
+    return total;
+};

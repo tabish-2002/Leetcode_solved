86982ee9e8a38da95b9e1cf4fad74b4e1d7f15fe
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2026-03-14 10:59:45 -0400

    Time: 0 ms (100%), Space: 58.9 MB (25.8%) - LeetHub

--- 0112-path-sum/0112-path-sum.js
+++ 0112-path-sum/0112-path-sum.js
@@ -12,12 +12,12 @@
  * @return {boolean}
  */
 var hasPathSum = function(root, targetSum) {
-    if (!root) return false;
+    if (! root) return false;
 
-    if (!root.left && !root.right && targetSum - root.val === 0) return true;
+    if (!root.left && !root.right && targetSum - root.val === 0) return true
 
- return (
-    hasPathSum(root.left, targetSum - root.val) ||
-    hasPathSum(root.right, targetSum - root.val)
- )   
+    return (
+        hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val) 
+    )
+    
 };

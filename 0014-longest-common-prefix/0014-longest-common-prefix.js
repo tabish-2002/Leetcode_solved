5b74b82838e5150482ac8f9498eb8e46a051df15
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2026-06-24 21:55:55 -0400

    Time: 0 ms (100%), Space: 58.1 MB (5.18%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -3,20 +3,17 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-        if(strs.length === 0) return "";
-    
+     if (strs.length === 0) return "";
+
     for (let i = 0; i < strs[0].length; i++) {
-        let char = strs[0][i]; // Get character from first string
-        
-        // Check this character against ALL other strings
+        let char = strs[0][i];
+
         for (let s of strs) {
             if (i >= s.length || s[i] !== char) {
-                // Mismatch found or string too short
                 return strs[0].slice(0, i);
             }
         }
     }
-    
-    return strs[0]; // All of first string is common prefix
-    
+
+    return strs[0];
 };

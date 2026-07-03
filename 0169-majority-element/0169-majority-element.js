1c2e4dfd78099eade4a5d7e7e22fdc03440a4153
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2026-07-03 16:11:11 -0400

    Time: 2 ms (73.67%), Space: 56 MB (84.33%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -1,13 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 var majorityElement = function(nums) {
         let map = new Map();
     const majority = Math.floor(nums.length / 2);
 
     for (let i = 0; i < nums.length; i++) {
         map.set(nums[i], (map.get(nums[i]) || 0) + 1);
-        
-        // Check immediately after updating
+
         if (map.get(nums[i]) > majority) {
             return nums[i];
         }
     }
+
 };

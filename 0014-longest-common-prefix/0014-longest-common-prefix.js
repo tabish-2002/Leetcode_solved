b643238f0c1044d3dbdcd0dc231dbcaa6d5f61b7
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2026-07-04 22:22:50 -0400

    Time: 4 ms (24.75%), Space: 58.3 MB (5.09%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -3,18 +3,16 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-        if (strs.length === 0) return "";
+    if (strs.length === 0) return "";
 
-    for (let i = 0; i < strs[0].length; i++) {
+    for (let i = 0; i<strs[0].length; i ++){
         let char = strs[0][i];
 
-        for (let s of strs) {
-            if (i >= s.length || s[i] !== char) {
-                return strs[0].slice(0, i);
+        for(let s of strs){
+            if (i>= s.length || s[i] !==char){
+                return strs[0].slice (0,i);
             }
         }
     }
-
     return strs[0];
-    
 };

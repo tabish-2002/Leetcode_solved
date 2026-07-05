366fb86d4ee2c0bac7350b39c76afc70480c75fd
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2026-07-04 22:18:13 -0400

    Time: 15 ms (51.17%), Space: 60.9 MB (13.56%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -5,19 +5,20 @@
  */
 var isAnagram = function(s, t) {
     if (s.length !== t.length){
-        return false
+        return false;
     }
     let map = new Map();
 
     for (let ch of s){
-        map.set(ch, (map.get(ch)||0)+1)
+        map.set(ch,(map.get(ch)||0)+1)
     }
     for (let ch of t){
-        if(!map.has(ch))return false;
-        map.set(ch, map.get(ch)-1)
-        if (map.get(ch) === 0){
-        map.delete(ch)
+        if(!map.has(ch)) return false;
+        map.set(ch, map.get(ch)-1);
+
+        if (map.get(ch)==0){
+            map.delete(ch)
         }
     }
-    return map.size === 0;
+    return (map.size === 0);
 };

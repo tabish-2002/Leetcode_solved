355cf5abd982e9550622b44341574f4d302ff777
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2026-07-05 22:38:07 -0400

    Time: 0 ms (100%), Space: 53.4 MB (77.67%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -4,13 +4,15 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    let k = 0;// set it to first calue for a pointer k
+    let k = 0;// set up the initial pointer at the first value
 
+    // go thru the values of nums
     for(let i = 0; i<nums.length; i++){
-        if(nums[i] !== val){
-            nums[k] = nums[i];
-            k++;
+        if(nums[i] !== val){// if the values (deonoted by [] because it calls the number) are not val
+            nums[k] = nums[i];// set the values of k to be i, what is the difference between = and ===
+            k++;// keep pushing the values of k
         }
     }
-    return k;
+    return k; // return the values of k
+    
 };

45dcf77ebec2beda4cfbcea2a34070f26784e2e0
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2026-07-08 16:40:16 -0400

    Time: 4 ms (76.99%), Space: 57.2 MB (73%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,22 +3,18 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-      // 1. Normalize: lowercase + remove non-alphanumeric
-    s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
-    
-    // 2. Initialize pointers
+        s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+
     let left = 0;
     let right = s.length - 1;
-    
-    // 3. Compare from both ends
+
     while (left < right) {
         if (s[left] !== s[right]) {
-            return false;  // mismatch
+            return false;
         }
         left++;
         right--;
     }
-    
-    // 4. If loop finishes, it's a palindrome
+
     return true;
 };

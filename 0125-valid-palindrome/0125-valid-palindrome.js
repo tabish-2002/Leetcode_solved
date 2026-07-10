dfcc9320fa13fb812dcd494ea53b4b6533ccc0ac
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2026-07-09 20:05:16 -0400

    Time: 1 ms (98.34%), Space: 57.2 MB (74.02%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,17 +3,17 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    s= s.toLowerCase().replace(/[^a-z0-9]/g,'');
+    s = s.toLowerCase().replace(/[^a-z0-9]/g,'');
 
     let left = 0;
     let right = s.length -1;
 
-    while (left < right){
-        if (s[left] !== s[right]){
-            return false;
+    while (left<right){
+        if(s[left] !== s[right]){
+            return false
         }
-        left ++;
-        right --;
+         left ++;
+         right --;
     }
     return true;
 };

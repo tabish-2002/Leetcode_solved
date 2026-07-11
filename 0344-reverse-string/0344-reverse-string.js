62d89bd5389789663057445e0965ca125e6aec70
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2026-07-11 14:37:20 -0400

    Time: 0 ms (100%), Space: 59 MB (98.93%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -6,10 +6,11 @@ var reverseString = function(s) {
     let left = 0;
     let right = s.length -1;
 
-    while(left<right){
-        let  temp = s[left];
+    while (left<right){
+        let temp = s[left];
         s[left] = s[right];
-        s[right] = temp;// should just be temp not values of temp
+        s[right] = temp;
+
         left ++;
         right --;
     }

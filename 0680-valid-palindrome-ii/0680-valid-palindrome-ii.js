e5e23f67134ce0a452a561b91b00bf364b33981a
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2026-07-13 12:26:49 -0400

    Time: 4 ms (96.05%), Space: 59.9 MB (92.41%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
@@ -5,27 +5,27 @@
 var validPalindrome = function(s) {
     if (s.length < 1) return true;
 
-    // Helper to check if a substring is a palindrome
-    const isPalindrome = (left, right) => {
-        while (left < right) {
-            if (s[left] !== s[right]) return false;
-            left++;
-            right--;
-        }
-        return true;
-    };
-
     let left = 0, right = s.length - 1;
 
     while (left < right) {
         if (s[left] !== s[right]) {
-            // Try skipping either the left or right character
-            return isPalindrome(left + 1, right) || isPalindrome(left, right - 1);
+            let l1 = left + 1, r1 = right, ok1 = true;
+            while (l1 < r1) {
+                if (s[l1] !== s[r1]) { ok1 = false; break; }
+                l1++; r1--;
+            }
+
+            let l2 = left, r2 = right - 1, ok2 = true;
+            while (l2 < r2) {
+                if (s[l2] !== s[r2]) { ok2 = false; break; }
+                l2++; r2--;
+            }
+
+            return ok1 || ok2;
         }
         left++;
         right--;
     }
 
     return true;
-    
 };

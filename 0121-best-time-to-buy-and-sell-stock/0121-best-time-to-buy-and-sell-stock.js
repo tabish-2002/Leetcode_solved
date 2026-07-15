40fcb8a86a78392e033d723760f9577e766f5e4f
Syed Tabish Rahman <107960761+tabish-2002@users.noreply.github.com>
2026-07-15 12:38:27 -0400

    Time: 7 ms (10.89%), Space: 65.4 MB (42.39%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,19 +3,17 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let left = 0;           // buy day
-    let right = 1;          // sell day
+    let left = 0;
+    let right = 1;
     let maxProfit = 0;
 
-    while (right < prices.length) {
-        if (prices[right] > prices[left]) {
-            maxProfit = Math.max(maxProfit, prices[right] - prices[left]);
-        } else {
-            left = right; // new minimum price to buy
+    while (right < prices.length){
+        if(prices[right] > prices[left]){
+            maxProfit = Math.max(maxProfit, prices[right] - prices[left])
+        }else{
+            left = right;
         }
-        right++;
+        right ++;
     }
-
     return maxProfit;
-    
 };
